bb70d676d15b9a6a78a0e1565b34ce3366989981
Caleb Aviles <earthctzn1@gmail.com>
2019-05-16 03:07:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   
   def self.new_from_filename(file_data)
     x = file_data.split(/\.|\s-\s/).reject{|word| word == "mp3"}
-    self.new_by_name(x[0])
+    self.new_by_name(x[1])
     # @artist_name == x[0]
     # @name << x[1]
     #binding.pry 
