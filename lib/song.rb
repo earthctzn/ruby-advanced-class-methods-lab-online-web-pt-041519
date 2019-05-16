6634e41a1e4c4e83908047e655ea0f8a1f11ddd4
Caleb Aviles <earthctzn1@gmail.com>
2019-05-16 03:50:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,6 +51,15 @@ class Song
     song
   end
   
+   def self.new_from_filename(file_data)
+    x = file_data.split(/\.|\s-\s/).reject{|word| word == "mp3"}
+    name = x[1]
+    artist_name = x[0]
+    song = self.new_by_name(name)
+    song.artist_name = artist_name
+    song
+  end
+  
   def self.destroy_all
     self.all.clear
   end
