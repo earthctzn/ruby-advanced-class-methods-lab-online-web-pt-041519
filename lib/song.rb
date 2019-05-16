9b0987822dfe6621a2c52267adfe8111dc5b6bbd
Caleb Aviles <earthctzn1@gmail.com>
2019-05-16 03:00:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,8 @@ class Song
   
   def self.new_from_filename(file_data)
     x = file_data.split(/\.|\s-\s/).reject{|word| word == "mp3"}
-    
+    artist_name = x[0]
+    name = x[1]
     binding.pry 
   end
   
